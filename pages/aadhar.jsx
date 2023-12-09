91767a2a453d3e27175342bd55f6259f06df8e5b
Ayush Jain <ayushjain2205@gmail.com>
2023-12-10 02:15:49 +0530

    After aadhar verification go to show persona

--- pages/aadhar.jsx
+++ pages/aadhar.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 import {
   LogInWithAnonAadhaar,
   useAnonAadhaar,
@@ -7,17 +8,31 @@ import {
 
 const Aadhar = () => {
   const [anonAadhaar] = useAnonAadhaar();
+  const router = useRouter();
 
   useEffect(() => {
+    // Log the Aadhaar status whenever it changes
     console.log("Anon Aadhaar status: ", anonAadhaar.status);
-  }, [anonAadhaar]);
+
+    // If the user is logged-in, navigate to '/feed' after 5 seconds
+    if (anonAadhaar.status === "logged-in") {
+      const timer = setTimeout(() => {
+        router.push("/persona");
+      }, 5000);
+
+      // Clear the timeout if the component unmounts
+      // to prevent a state update on an unmounted component
+      return () => clearTimeout(timer);
+    }
+  }, [anonAadhaar.status, router]);
+
   return (
     <div className="flex flex-col px-[27px]">
       <p className="text-[20px] font-[500] mt-[80px]">
         Aadhar Card Verification
       </p>
       <p className="text-[12px]">Verify on a click</p>
-      <img className="mt-[80px]" src="/images/aadhar.png" alt="" />
+      <img className="mt-[80px]" src="/images/aadhar.png" alt="Aadhar card" />
       <div className="login-btn mt-[70px]">
         <LogInWithAnonAadhaar />
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,26 @@
-import { useLogin } from "@privy-io/react-auth";
-import Head from "next/head";
+import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 
-export default function LoginPage() {
+const Landing = () => {
   const router = useRouter();
-  const { login } = useLogin({
-    onComplete: () => router.push("/aadhar"),
-  });
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/landing");
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
 
   return (
-    <>
-      <Head>
-        <title>Jan Protocol</title>
-      </Head>
-
-      <main className="flex flex-col h-screen items-center">
-        <div>
-          <img
-            className="w-[390px] mt-[100px] h-[250px] animate-pulse"
-            src="/images/landing.png"
-            alt=""
-          />
-          <div className="mt-[90px] flex justify-center text-center">
-            <button
-              className="w-[336px] h-[46px] bg-primary text-white rounded-[8px]"
-              onClick={login}
-            >
-              Log in
-            </button>
-          </div>
-        </div>
-      </main>
-    </>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <img
+        className="w-[390px] h-[250px] -mt-[100px] animate-pulse"
+        src="/images/landing.png"
+        alt=""
+      />
+    </div>
   );
-}
+};
+
+export default Landing;
