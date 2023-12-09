bb7c97d7c069bf1e0b09c26e91184e5a1ff84e5d
Ayush Jain <ayushjain2205@gmail.com>
2023-12-10 03:21:17 +0530

    Fix select slot

--- pages/schedule.jsx
+++ pages/schedule.jsx
@@ -1,7 +1,16 @@
+// @ts-nocheck
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const feed = () => {
+  const [selectedTime, setSelectedTime] = useState(null);
+
+  const timeSlots = ["8:00", "10:00", "13:00"]; // Add as many timeslots as needed
+
+  const handleTimeSelect = (time) => {
+    setSelectedTime(time);
+  };
+
   return (
     <div className="flex flex-col px-[27px] py-[30px]">
       <div className="flex flex-row justify-between">
@@ -21,7 +30,7 @@ const feed = () => {
         </div>
 
         <div className="flex flex-row justify-between items-center w-[336px] h-[48px] border border-black rounded-[6px] mt-[8px] px-[8px]">
-          <select className="w-[336px] border-none appearance-none">
+          <select className="w-[336px] border-none appearance-none outline-none focus:outline-none">
             <option value="option1">House cleaning</option>
             <option value="option2">Cooking</option>
             <option value="option3">Baby Sitting</option>
@@ -42,29 +51,31 @@ const feed = () => {
         </div>
 
         <div className="flex flex-row justify-between gap-[20px]">
-          <button className="w-[94px] h-[40px] rounded-[6px] bg-white border border-black mt-[10px] font-regular text-[14px]">
-            <p className="text-[#000000]">8:00</p>
-          </button>
-
-          <button className="w-[94px] h-[40px] rounded-[6px] bg-white border border-black mt-[10px] text-[14px] ">
-            <p className="text-[#000000]">10:00</p>
-          </button>
-
-          <button className="w-[94px] h-[40px] rounded-[6px] bg-white border border-black mt-[10px] font-regular text-[14px]">
-            <p className="text-[#000000]">13:00</p>
-          </button>
+          {timeSlots.map((time) => (
+            <button
+              key={time}
+              onClick={() => handleTimeSelect(time)}
+              className={`w-[94px] h-[40px] rounded-[6px] ${
+                selectedTime === time
+                  ? "bg-primary text-white"
+                  : "bg-white text-black"
+              } border border-black mt-[10px] font-regular text-[14px]`}
+            >
+              {time}
+            </button>
+          ))}
         </div>
 
         <div>
-          <button className="w-[336px] h-[40px] rounded-[6px] bg-[#FC7904] mt-[60px] font-regular text-[14px]">
-            <p className="text-[#FFFFFF]">Pay now</p>
+          <button className="w-[336px] h-[46px] rounded-[6px] bg-[#FC7904] mt-[60px] font-regular text-[14px]">
+            <p className="text-[#FFFFFF] text-[16px] font-[500]">Pay now</p>
           </button>
         </div>
 
         <div>
           <Link href="/otp">
             <button className="w-[336px] h-[40px] rounded-[6px] bg-white mt-[20px] underline text-[14px]">
-              <p className="text-[#000000]">Pay after service</p>
+              <p className="text-[#000000] text-[16px]">Pay after service</p>
             </button>
           </Link>
         </div>
